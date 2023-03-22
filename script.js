bf70ab8d015e532eb89691b0f112ff520c92740b
Corey Kovach <124169155+CoreyK4@users.noreply.github.com>
2023-03-22 08:49:07 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -15,5 +15,26 @@ function getComputerChoice() {
   }
 }
 //Create a function to play a single round and return the result
+function playRound(playerSelection, computerSelection) {
+  let isTie = playerSelection === computerSelection;
+  playerSelection = playerSelection.toLowerCase();
 
+  if (isTie) {
+    return "It's a tie!";
+  } else if (playerSelection === "rock") {
+    return computerSelection === "paper"
+      ? "You Lose! Paper beats rock!"
+      : "You Win! Rock beats scissors!";
+  } else if (playerSelection === "paper") {
+    return computerSelection === "rock"
+      ? "You Win! Paper beats rock!"
+      : "You Lose! Scissors beat paper!";
+  } else if (playerSelection === "scissors") {
+    return computerSelection === "rock"
+      ? "You Lose! Rock beats scissors!"
+      : "You Win! Scissors beat paper!";
+  } else {
+    return "You have entered an invalid choice!";
+  }
+}
 //Create a function to play a 5 round game
